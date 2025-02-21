1391ca7c21d99d06a36fe79ae56ead06cafade5b
Ria Arora <riaarora@Rias-MacBook-Air.local>
2025-02-21 15:58:20 +0530

    working authentication

---
+++ app/test/page.jsx
@@ -0,0 +1,24 @@
+'use client'
+import { ChatWithRent, TestDB } from '@/server/openai/action'
+import { Button } from '@heroui/react'
+import React, {useState} from 'react'
+import Header from '../components/Header'
+
+function page() {
+    const [prompt, setprompt] = useState("")
+    async function call(){
+        const res = await ChatWithRent(prompt)
+        console.log(res)
+    }
+
+
+  return (
+    <div>
+        <Header/>
+      <input className="text-black" type="text" onChange={(e)=>setprompt(e.target.value)} value={prompt}/>
+      <Button onPress={call}>Call</Button>
+    </div>
+  )
+}
+
+export default page
