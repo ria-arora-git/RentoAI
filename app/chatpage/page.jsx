07709ee2ad25267e5044215f0b4fe4125389c01a
Namiiii-sys <Namitamehra207@gmail.com>
2025-02-21 14:04:20 +0530

    ai-page-first-base

---
+++ app/chatpage/page.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function chatbot() {
+  return (
+    <div className="flex justify-center items-center gap-4 h-screen">
+      <div className="bg-purple-50 w-1/4 h-4/5 flex justify-around items-center">
+        <p></p>
+      <div className="bg-black h-5/6 w-3/5">
+
+      </div>
+      </div>
+      <div className="bg-purple-50 w-3/5 h-4/5">
+      <div className=" flex justify-center justify-items-end">
+        <div className="bg-red-100 h-80">
+
+        </div>
+        <div className="w-4/5 p-8 h-10 bg-blue-200 rounded-lg ">
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default chatbot
